020d20cd9b4c306ceaf4e08a948152a682f53451
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-05-28 18:17:17 -0300

    🚨 Fix offenses

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,6 +1,6 @@
 class Api::GreetingsController < ApplicationController
   def random_greeting
-    greeting = Message.order("RANDOM()").last
+    greeting = Message.order('RANDOM()').last
     render json: { greeting: greeting.greeting }
   end
 end
